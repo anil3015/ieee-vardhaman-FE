61eda9633a3ff36d3b091e29b55c3485280f9308
achiever2004 <ayilvarrajkumar@gmail.com>
2025-06-27 00:47:23 +0530

    firestoreconfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYjzj-mqSHj0y51r7YCZdBMRUqJqF4zTQ",
-  authDomain: "ieee-vmeg.firebaseapp.com",
-  projectId: "ieee-vmeg",
-  storageBucket: "ieee-vmeg.firebasestorage.app",
-  messagingSenderId: "870765401169",
-  appId: "1:870765401169:web:c65659082df0ae5de2d470",
-  measurementId: "G-JTD48F511G"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
